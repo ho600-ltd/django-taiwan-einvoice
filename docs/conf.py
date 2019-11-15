cefc740ef87dc621c26bdc43a03d8a7fe95fb625
hoamon <hoamon@ho600.com>
2019-11-15 09:32:55 +0800

    import todo module @CMEC2-324-hoamon-task-build-taiwan-einvoice-service-for-cm]

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,11 @@ release = '0.0'
 # ones.
 extensions = [
     'sphinx_rtd_theme',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
 ]
+
+todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
