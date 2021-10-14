42038d57b9c7c29e4ece696965967a8fef13ee98
hoamon h97progamer <hoamon@ho600.com>
2021-10-14 13:49:31 +0800

    It can set the default ESC/POS Web printer now @CMEC2-324-hoamon-task-202107-build-taiwan-einvoice-service-for-cm]

--- taiwan_einvoice/static/taiwan_einvoice/escposweb_list.js
+++ taiwan_einvoice/static/taiwan_einvoice/escposweb_list.js
@@ -10,11 +10,30 @@ $(function () {
 
     adjust_pagination_html();
 
-    $(".search").click(function () {
+    $("button.search").click(function () {
         var url_parts = window.location.href.split('?');
         var params = new URLSearchParams($('form:not(".language_form")').serialize());
 
         var result_url = url_parts[0] + '?' + params.toString();
         window.location.href = result_url;
+    });
+
+    var cookie_name = 'default_escposweb_printer';
+    var show_default_escposweb_printer = function () {
+        var default_escposweb_printer_id = $.cookie(cookie_name);
+        $('table tbody td[field=usage]').text('');
+        if (default_escposweb_printer_id) {
+            var usage_message = gettext('Default ESC/POS Printer');
+            $('table tbody tr[escposweb_id='+default_escposweb_printer_id+'] td[field=usage]').text(usage_message);
+        };
+    };
+    show_default_escposweb_printer();
+
+    $('button.set_default_escposweb_printer').click(function(){
+        var $btn = $(this);
+        var $tr = $btn.parents('tr');
+        var default_escposweb_printer_id = $tr.attr('escposweb_id');
+        $.cookie(cookie_name, default_escposweb_printer_id);
+        show_default_escposweb_printer();
     });
 });
