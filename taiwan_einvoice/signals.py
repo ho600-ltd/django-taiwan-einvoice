f848d6b6df69b3df816fee0758f645848d644b1d
hoamon h97progamer <hoamon@ho600.com>
2021-11-22 19:07:18 +0800

    Only update ucel, if user @CMEC2-324-hoamon-task-202107-build-taiwan-einvoice-service-for-cm]

--- taiwan_einvoice/signals.py
+++ taiwan_einvoice/signals.py
@@ -9,19 +9,20 @@ from taiwan_einvoice.models import UserConnectESCPOSWebLog
 @receiver(user_logged_out)
 def on_user_logout(sender, request, user, **kwargs):
     channel_layer = get_channel_layer()
-    for ucel in user.userconnectescposweblog_set.filter(is_connected=True):
-        group_name = "escpos_web_status_{}".format(ucel.escpos_web.id)
-        error_message_dict = {
-                'type': 'show_error_message',
-                'error_message': {"error_message": {"value": _("Already Logout")}},
-            }
-        async_to_sync(channel_layer.group_send)(
-            group_name,
-            error_message_dict
-        )
-        async_to_sync(channel_layer.group_discard)(
-            group_name,
-            ucel.channel_name
-        )
-        ucel.is_connected = False
-        ucel.save()
+    if user:
+        for ucel in user.userconnectescposweblog_set.filter(is_connected=True):
+            group_name = "escpos_web_status_{}".format(ucel.escpos_web.id)
+            error_message_dict = {
+                    'type': 'show_error_message',
+                    'error_message': {"error_message": {"value": _("Already Logout")}},
+                }
+            async_to_sync(channel_layer.group_send)(
+                group_name,
+                error_message_dict
+            )
+            async_to_sync(channel_layer.group_discard)(
+                group_name,
+                ucel.channel_name
+            )
+            ucel.is_connected = False
+            ucel.save()
