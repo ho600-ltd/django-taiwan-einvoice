f00a4ab64a5e41ced394092695ba295f264fe2bc
hoamon h97progamer <hoamon@ho600.com>
2021-12-12 17:23:47 +0800

    print=>logging @CMEC2-595-hoamon-task-20211118-set-up-lcd-on-the-raspberrypi]

--- escpos_web/lcd_control.py
+++ escpos_web/lcd_control.py
@@ -5,8 +5,12 @@ import LCD_Config
 
 import RPi.GPIO as GPIO
 
-import time
+import time, logging
 from PIL import Image,ImageDraw,ImageFont,ImageColor
+
+logging.basicConfig()
+lg = logging.getLogger(__name__)
+lg.setLevel('INFO')
 
 KEY_UP_PIN     = 6 
 KEY_DOWN_PIN   = 19
@@ -53,52 +57,52 @@ while 1:
     # with canvas(device) as draw:
     if GPIO.input(KEY_UP_PIN) == 0: # button is released       
         draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0xff00)  #Up        
-        print("Up")
+        lg.info("Up")
     else: # button is pressed:
         draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  #Up filled
         
     if GPIO.input(KEY_LEFT_PIN) == 0: # button is released
         draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0xff00)  #left
-        print("left")
+        lg.info("left")
     else: # button is pressed:       
         draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left filled
         
     if GPIO.input(KEY_RIGHT_PIN) == 0: # button is released
         draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0xff00) #right
-        print("right")
+        lg.info("right")
     else: # button is pressed:
         draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right filled       
         
     if GPIO.input(KEY_DOWN_PIN) == 0: # button is released
         draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0xff00) #down
-        print("down")
+        lg.info("down")
     else: # button is pressed:
         draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0) #down filled
         
     if GPIO.input(KEY_PRESS_PIN) == 0: # button is released
         draw.rectangle((20, 22,40,40), outline=255, fill=0xff00) #center 
-        print("center")
+        lg.info("center")
     else: # button is pressed:
         draw.rectangle((20, 22,40,40), outline=255, fill=0) #center filled
         
     if GPIO.input(KEY1_PIN) == 0: # button is released
         draw.ellipse((70,0,90,20), outline=255, fill=0xff00) #A button
-        print("KEY1")
+        lg.info("KEY1")
     else: # button is pressed:
         draw.ellipse((70,0,90,20), outline=255, fill=0) #A button filled
         
     if GPIO.input(KEY2_PIN) == 0: # button is released
         draw.ellipse((100,20,120,40), outline=255, fill=0xff00) #B button]
-        print("KEY2")
+        lg.info("KEY2")
     else: # button is pressed:
         draw.ellipse((100,20,120,40), outline=255, fill=0) #B button filled
         
     if GPIO.input(KEY3_PIN) == 0: # button is released
         draw.ellipse((70,40,90,60), outline=255, fill=0xff00) #A button
-        print("KEY3")
+        lg.info("KEY3")
     else: # button is pressed:
         draw.ellipse((70,40,90,60), outline=255, fill=0) #A button filled
     disp.LCD_ShowImage(image,0,0)
 # except:
-	# print("except")
+	# lg.info("except")
     # GPIO.cleanup()
