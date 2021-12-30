3db98e7c1f60b7a2b9d71f31de793fe940ec38f2
hoamon h97progamer <hoamon@ho600.com>
2021-12-30 11:12:24 +0800

    Superuser can write LegalEntity.identifier @CMEC2-324-hoamon-task-202107-build-taiwan-einvoice-service-for-cm]

--- taiwan_einvoice/serializers.py
+++ taiwan_einvoice/serializers.py
@@ -73,7 +73,7 @@ class PrinterSerializer(ModelSerializer):
 
 
 
-class LegalEntitySerializer(ModelSerializer):
+class LegalEntitySerializerForUser(ModelSerializer):
     resource_uri = HyperlinkedIdentityField(
         view_name="taiwan_einvoice:taiwaneinvoiceapi:legalentity-detail", lookup_field='pk')
     identifier = ReadOnlyField()
@@ -93,10 +93,15 @@ class LegalEntitySerializer(ModelSerializer):
 
 
 
+class LegalEntitySerializerForSuperUser(LegalEntitySerializerForUser):
+    identifier = CharField()
+
+
+
 class SellerSerializer(ModelSerializer):
     resource_uri = HyperlinkedIdentityField(
         view_name="taiwan_einvoice:taiwaneinvoiceapi:seller-detail", lookup_field='pk')
-    legal_entity_dict = LegalEntitySerializer(source='legal_entity', read_only=True)
+    legal_entity_dict = LegalEntitySerializerForUser(source='legal_entity', read_only=True)
 
     class Meta:
         model = Seller
