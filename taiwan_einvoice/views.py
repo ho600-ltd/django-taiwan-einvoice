fad814bab99873614c374476368254e2934644b4
hoamon h97progamer <hoamon@ho600.com>
2022-08-13 14:41:54 +0800

    We can only write the handling note without creating the new upload_batch record @CMEC2-635-hoamon-task-build-turnkey-service-with-restframework]

--- taiwan_einvoice/views.py
+++ taiwan_einvoice/views.py
@@ -978,7 +978,7 @@ class BatchEInvoiceModelViewSet(ModelViewSet):
                 "error_message": _('Empty Hanling Note!')
             }
             return Response(er, status=status.HTTP_403_FORBIDDEN)
-        elif handling_type not in ["the_same_track_no", "with_new_track_no"]:
+        elif handling_type not in ["the_same_track_no", "with_new_track_no", "no_new_upload_batch"]:
             er = {
                 "error_title": _("Data Error"),
                 "error_message": _('Wrong Hanling Type!')
@@ -986,36 +986,41 @@ class BatchEInvoiceModelViewSet(ModelViewSet):
             return Response(er, status=status.HTTP_403_FORBIDDEN)
 
         content_object = batch_einvoice.content_object
-        if "the_same_track_no" == handling_type:
+        if "no_new_upload_batch" == handling_type:
+            kind = ''
+        elif "the_same_track_no" == handling_type:
             kind = 'R'
         elif "with_new_track_no" == handling_type:
             kind = 'RN'
             content_object = content_object.renew_track_no_and_sitn_obj()
 
-        i = 0 
-        while i < 10000:
-            slug = "{}{:04d}".format(content_object.track_no, i)
-            if not UploadBatch.objects.filter(slug=slug).exists():
-                break
-            i += 1
-        new_ub = UploadBatch(turnkey_service=batch_einvoice.batch.turnkey_service,
-                             slug=slug,
-                             mig_type=batch_einvoice.batch.mig_type,
-                             kind=kind,
-                             status='0')
-        new_ub.save()
-        new_be = BatchEInvoice(batch=new_ub,
-                               content_object=content_object,
-                               begin_time=content_object.seller_invoice_track_no.begin_time,
-                               end_time=content_object.seller_invoice_track_no.end_time,
-                               track_no=content_object.track_no,
-                               body="",
-                              )
-        new_be.save()
+        if '' == kind:
+            new_ub = False
+        else:
+            i = 0 
+            while i < 10000:
+                slug = "{}{:04d}".format(content_object.track_no, i)
+                if not UploadBatch.objects.filter(slug=slug).exists():
+                    break
+                i += 1
+            new_ub = UploadBatch(turnkey_service=batch_einvoice.batch.turnkey_service,
+                                slug=slug,
+                                mig_type=batch_einvoice.batch.mig_type,
+                                kind=kind,
+                                status='0')
+            new_ub.save()
+            new_be = BatchEInvoice(batch=new_ub,
+                                content_object=content_object,
+                                begin_time=content_object.seller_invoice_track_no.begin_time,
+                                end_time=content_object.seller_invoice_track_no.end_time,
+                                track_no=content_object.track_no,
+                                body="",
+                                )
+            new_be.save()
         batch_einvoice.handling_note = handling_note
         batch_einvoice.pass_if_error = True
         batch_einvoice.save()
-        return Response({"slug": new_ub.slug}, status=status.HTTP_201_CREATED)
+        return Response({"slug": new_ub.slug if new_ub else _("None")}, status=status.HTTP_201_CREATED)
 
 
 class AuditLogModelViewSet(ModelViewSet):
