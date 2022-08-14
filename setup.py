dda71446c53737a7296ec6c1f4875d70e9d96b0c
hoamon h97progamer <hoamon@ho600.com>
2022-08-14 11:21:51 +0800

    0.0.52-5 @dev-deploy-01]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [r for r in REQUIREMENTS.split('\n') if r
 
 setup(name='taiwan_einvoice',
       install_requires=install_requires,
-      version='0.0.52-4',
+      version='0.0.52-5',
       packages=[
                 'taiwan_einvoice',
                 'taiwan_einvoice.migrations',
