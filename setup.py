8ea4671a4d3fe16677b6e3aa54ae463a62dcc384
hoamon h97progamer <hoamon@ho600.com>
2022-08-24 18:00:24 +0800

    0.1.1 @CMEC2-690-master]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [r for r in REQUIREMENTS.split('\n') if r
 
 setup(name='taiwan_einvoice',
       install_requires=install_requires,
-      version='0.1.0',
+      version='0.1.1',
       packages=[
                 'taiwan_einvoice',
                 'taiwan_einvoice.migrations',
