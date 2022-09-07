534ed2ddcd0fa9569cbaf96c7dedcb542f26789e
hoamon h97progamer <hoamon@ho600.com>
2022-09-07 09:30:53 +0800

    Update mena @CMEC2-690-master]

--- turnkey_web/turnkey_wrapper/static/turnkey_wrapper/eiturnkeybatch_list.js
+++ turnkey_web/turnkey_wrapper/static/turnkey_wrapper/eiturnkeybatch_list.js
@@ -1,7 +1,5 @@
 $(function () {
-    $(".nav_operation").addClass("nav_active");
-    $('.dropdown-menu a').removeClass('active');
-    $(".nav_EITurnkeyBatch").addClass("active");
+    $(".nav_EITurnkeyBatch").addClass("nav_active");
 
 
     turnkey_wrapper_site = new turnkey_wrapper_site('turnkey_wrapper_site', {
