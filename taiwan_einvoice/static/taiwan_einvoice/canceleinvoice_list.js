36ab79c625517a0045aa19e886c8024987b37eba
hoamon h97progamer <hoamon@ho600.com>
2022-10-14 09:45:41 +0800

    show_canceleinvoice_modal=>show_executing_canceleinvoice_modal @CMEC2-690-master]

--- taiwan_einvoice/static/taiwan_einvoice/canceleinvoice_list.js
+++ taiwan_einvoice/static/taiwan_einvoice/canceleinvoice_list.js
@@ -1,4 +1,4 @@
-function show_canceleinvoice_modal(taiwan_einvoice_site) {
+function show_executing_canceleinvoice_modal(taiwan_einvoice_site) {
     return function () {
         var $btn = $(this);
         var $form = $btn.parents('form');
@@ -11,7 +11,7 @@ function show_canceleinvoice_modal(taiwan_einvoice_site) {
                 );
             return false;
         }
-        var $modal = $('#show_canceleinvoice_modal');
+        var $modal = $('#show_executing_canceleinvoice_modal');
         var resource_uri_tmpl = $modal.attr('resource_uri_tmpl');
         var resource_uri = resource_uri_tmpl.replace('{one_dimensional_barcode_for_canceling}', one_dimensional_barcode_for_canceling);
         $.ajax({
@@ -271,6 +271,6 @@ $(function () {
         });
     }
 
-    $('button.show_canceleinvoice_modal').click(show_canceleinvoice_modal(taiwan_einvoice_site));
+    $('button.show_executing_canceleinvoice_modal').click(show_executing_canceleinvoice_modal(taiwan_einvoice_site));
     $('button.cancel_einvoice').click(cancel_einvoice(taiwan_einvoice_site));
 });
