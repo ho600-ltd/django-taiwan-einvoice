33382bedd18f2527e2abde18f478aac75a06acfb
hoamon macos <hoamon@ho600.com>
2022-12-20 08:43:39 +0800

    0.3.6 @master]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_static_files(directory):
 
 setup(name='taiwan_einvoice',
       install_requires=install_requires,
-      version='0.3.5',
+      version='0.3.6',
       packages=[
                 'taiwan_einvoice',
                 'taiwan_einvoice.migrations',
