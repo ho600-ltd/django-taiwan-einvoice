372b3c5eeb57589d7e71dbed8dc1331ccc41164e
hoamon(R9-69-00) <hoamon@ho600.com>
2023-07-20 12:08:14 +0800

    0.3.26 @master]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_static_files(directory):
 
 setup(name='taiwan_einvoice',
       install_requires=install_requires,
-      version='0.3.25',
+      version='0.3.26',
       packages=[
                 'taiwan_einvoice',
                 'taiwan_einvoice.migrations',
