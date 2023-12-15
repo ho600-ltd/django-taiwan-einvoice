630a475909fbeb4ee7953176ac1ba9258f4c4add
hoamon(R9-69-00) <hoamon@ho600.com>
2023-12-15 13:47:02 +0800

    Use a ugly way to void redis server downtime @CMEC2-847-hoamon-task-create-aws-elasticache-redis-serverless-endpoint]

--- taiwan_einvoice/signals.py
+++ taiwan_einvoice/signals.py
@@ -6,23 +6,31 @@ from channels.layers import get_channel_layer
 from taiwan_einvoice.models import UserConnectESCPOSWebLog
 
 
+
+def discard_group_after_send_message(channel_layer, ucel):
+    group_name = "escpos_web_status_{}".format(ucel.escpos_web.id)
+    error_message_dict = {
+            'type': 'show_error_message',
+            'error_message': {"error_message": {"value": _("Already Logout")}},
+        }
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        error_message_dict
+    )
+    async_to_sync(channel_layer.group_discard)(
+        group_name,
+        ucel.channel_name
+    )
+
+
 @receiver(user_logged_out)
 def on_user_logout(sender, request, user, **kwargs):
     channel_layer = get_channel_layer()
     if user:
         for ucel in user.userconnectescposweblog_set.filter(is_connected=True):
-            group_name = "escpos_web_status_{}".format(ucel.escpos_web.id)
-            error_message_dict = {
-                    'type': 'show_error_message',
-                    'error_message': {"error_message": {"value": _("Already Logout")}},
-                }
-            async_to_sync(channel_layer.group_send)(
-                group_name,
-                error_message_dict
-            )
-            async_to_sync(channel_layer.group_discard)(
-                group_name,
-                ucel.channel_name
-            )
+            try:
+                discard_group_after_send_message(channel_layer, ucel)
+            except:
+                pass
             ucel.is_connected = False
             ucel.save()
