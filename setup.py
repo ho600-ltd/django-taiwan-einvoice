152700dc426f944978a210f6a1a8051c0a2daf98
hoamon(R9-69-00) <hoamon@ho600.com>
2025-03-22 15:20:28 +0800

    0.9.21-1 @dev-deploy-01]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_static_files(directory):
 
 setup(name='taiwan_einvoice',
       install_requires=install_requires,
-      version='0.9.20-1',
+      version='0.9.21-1',
       packages=[
                 'taiwan_einvoice',
                 'taiwan_einvoice.migrations',
